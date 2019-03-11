fdf28603625319e6c149d5d508a446e22c7539df
indahlissi <48428993+indahlissi@users.noreply.github.com>
2019-03-11 14:16:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "HELLO WORD!";
+?> 
+
+<br>
+<?php
+echo "NAMA	: INDAH LISSIANA PUTRI";
+?>
+
+<br>
+<?php
+echo "NIM	: 16.01.53.0004";
+?>
+
+<br>
+<?php
+echo "PROGDI	: TEKNIK INFORMATIKA";
+?>
+
+</body>
+</html>
